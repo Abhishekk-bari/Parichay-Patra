a39d0326fd402318c931ee85a1f832e7b7723c2c
Abhi <abhi2bari929@gmail.com>
2024-11-01 09:41:05 +0530

    fuckedup in last commit that i done ex in animation phase now its clear

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,17 +3,18 @@ import '../fonts/NeueMontreal-Regular.ttf';
 import card from '../assets/card 3.png';
 import card2 from '../assets/card2.png';
 import '../fonts/FoundersGrotesk-Semibold.ttf';
-import { motion } from 'framer-motion';
-import { useState } from 'react';
-import { Power4 } from 'gsap/all'
-
-
+import { motion, useAnimation } from 'framer-motion';
 
 function Projects() {
+  const controls = [useAnimation(), useAnimation()];
 
-  const [isHovering, setHovering] = useState(false);
+  const handleHover = (index) => {
+    controls[index].start({ y: "0%", opacity: 1 });
+  };
 
-  
+  const handleHoverEnd = (index) => {
+    controls[index].start({ y: "100%", opacity: 0 });
+  };
 
   return (
     <div className='w-full py-20 bg-zinc-100'>
@@ -22,43 +23,57 @@ function Projects() {
       </div>
       <div className='px-20'>
         <div className="cards w-full flex gap-20 mt-10">
-            
-          <div 
-          onMouseEnter={()=>setHovering(true)} 
-          onMouseLeave={()=>setHovering(false)} 
-          className='cardcontainer relative w-1/2 h-[70vh]'>
-
-          <h1 className="absolute overflow-hidden flex left-full -translate-x-1/2 top-1/2 -translate-y-1/2 leading-none z-[9] uppercase text-[#CDEA68] font-['FoundersGroteskSemibold'] text-6xl">
-          {"TWEET_DECK".split('').map((item, index)=> (
-          <motion.span 
-          initial={{y: "100"}} 
-          animate={isHovering ? ({y: "0"}) : ({y: "100%"})} 
-          transition={{ease: Power4.easeInOut, delay:index*.1}}
-          className="inline-block translate-y-full">{item}</motion.span>))}
-          </h1>
-            
+          
+          {/* First Card */}
+          <motion.div 
+            onHoverStart={() => handleHover(0)} 
+            onHoverEnd={() => handleHoverEnd(0)} 
+            className='cardcontainer relative w-1/2 h-[70vh]'
+          >
+            <h1 className="absolute flex left-full -translate-x-1/2 top-1/2 -translate-y-1/2 leading-none z-[9] uppercase text-[#CDEA68] font-['FoundersGroteskSemibold'] text-6xl">
+              {"TWEET_DECK".split('').map((item, index) => (
+                <motion.span 
+                  initial={{ y: "100%", opacity: 0 }} 
+                  animate={controls[0]} 
+                  className="inline-block"
+                  key={index}
+                >
+                  {item}
+                </motion.span>
+              ))}
+            </h1>
             <div className='card w-full h-full rounded-xl overflow-hidden bg-zinc-300 relative'>
               <img className="w-full h-full object-cover" src={card} alt="Project Card" />
             </div>
-          </div>
+          </motion.div>
 
-          <div className='cardcontainer relative w-1/2 h-[70vh]'>
-
-          <h1 className="absolute right-full translate-x-1/2 top-1/2 -translate-y-1/2 leading-none z-[9] uppercase text-[#CDEA68] font-['FoundersGroteskSemibold'] text-6xl">
-          {"SAAS WEBSITE".split('').map((item, index)=> (
-          <span>{item}</span>))}
-          </h1>
+          {/* Second Card */}
+          <motion.div 
+            onHoverStart={() => handleHover(1)} 
+            onHoverEnd={() => handleHoverEnd(1)} 
+            className='cardcontainer relative w-1/2 h-[70vh]'
+          >
+            <h1 className="absolute flex right-full translate-x-1/2 top-1/2 -translate-y-1/2 leading-none z-[9] uppercase text-[#CDEA68] font-['FoundersGroteskSemibold'] text-6xl">
+              {"SAAS WEBSITE".split('').map((item, index) => (
+                <motion.span 
+                  initial={{ y: "100%", opacity: 0 }} 
+                  animate={controls[1]} 
+                  className="inline-block"
+                  key={index}
+                >
+                  {item}
+                </motion.span>
+              ))}
+            </h1>
             <div className='card w-full h-full rounded-lg overflow-hidden bg-zinc-300 relative'>
               <img className="w-full h-full object-cover" src={card2} alt="Card Image" />
             </div>
-          </div>
+          </motion.div>
 
         </div>
       </div>
     </div>
   );
 }
-
-
 
 export default Projects;
