4769faae8784bd624eeb00e498588a08936efcc0
Abhi <abhi2bari929@gmail.com>
2024-11-01 09:08:18 +0530

    done animation part of project also fix the h and w of images of cards

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 
 function Marquee() {
   return (
-    <div data-scroll data-scroll-section data-scroll-speed="-.3" className="w-full py-20 rounded-3xl bg-[#004D43]">
+    <div  className="w-full py-20 rounded-3xl bg-[#004D43]">
       <div className="text-white border-t-2 border-b-2 border-zinc-800 overflow-hidden flex whitespace-nowrap">
         <motion.h1 
           initial={{ x: "0" }} 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,40 +3,62 @@ import '../fonts/NeueMontreal-Regular.ttf';
 import card from '../assets/card 3.png';
 import card2 from '../assets/card2.png';
 import '../fonts/FoundersGrotesk-Semibold.ttf';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import { Power4 } from 'gsap/all'
 
 
 
 function Projects() {
+
+  const [isHovering, setHovering] = useState(false);
+
+  
+
   return (
     <div className='w-full py-20 bg-zinc-100'>
       <div className='w-full px-20 border-b-[1px] border-zinc-300 pb-10'>
         <h1 className="text-7xl font-['NeueMontrealRegular'] tracking-tight">Projects</h1>
       </div>
       <div className='px-20'>
-        <div className="cards w-full  flex gap-20 mt-10">
+        <div className="cards w-full flex gap-20 mt-10">
             
-          <div className='cardcontainer relative w-1/2 h-[70vh]'>
-          <h1 className="absolute left-full -translate-x-1/2 top-1/2 -translate-y-1/2 leading-none z-[9] uppercase text-[#CDEA68] font-['FoundersGroteskSemibold'] text-6xl">
+          <div 
+          onMouseEnter={()=>setHovering(true)} 
+          onMouseLeave={()=>setHovering(false)} 
+          className='cardcontainer relative w-1/2 h-[70vh]'>
+
+          <h1 className="absolute overflow-hidden flex left-full -translate-x-1/2 top-1/2 -translate-y-1/2 leading-none z-[9] uppercase text-[#CDEA68] font-['FoundersGroteskSemibold'] text-6xl">
           {"TWEET_DECK".split('').map((item, index)=> (
-          <span>{item}</span>))}
+          <motion.span 
+          initial={{y: "100"}} 
+          animate={isHovering ? ({y: "0"}) : ({y: "100%"})} 
+          transition={{ease: Power4.easeInOut, delay:index*.1}}
+          className="inline-block translate-y-full">{item}</motion.span>))}
           </h1>
+            
             <div className='card w-full h-full rounded-xl overflow-hidden bg-zinc-300 relative'>
-              <img className="w-full h-[80%] object-contain" src={card} alt="Project Card" />
+              <img className="w-full h-full object-cover" src={card} alt="Project Card" />
             </div>
           </div>
+
           <div className='cardcontainer relative w-1/2 h-[70vh]'>
+
           <h1 className="absolute right-full translate-x-1/2 top-1/2 -translate-y-1/2 leading-none z-[9] uppercase text-[#CDEA68] font-['FoundersGroteskSemibold'] text-6xl">
           {"SAAS WEBSITE".split('').map((item, index)=> (
           <span>{item}</span>))}
           </h1>
             <div className='card w-full h-full rounded-lg overflow-hidden bg-zinc-300 relative'>
-              <img className="w-full h-[80%] object-contain" src={card2} alt="Card Image" />
+              <img className="w-full h-full object-cover" src={card2} alt="Card Image" />
             </div>
           </div>
+
         </div>
       </div>
     </div>
   );
 }
+
+
 
 export default Projects;
