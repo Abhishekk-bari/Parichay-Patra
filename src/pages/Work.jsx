8ed39aa028f07adc637df2126122a5bd04335316
Abhi <abhi2bari929@gmail.com>
2024-12-07 16:01:40 +0530

    add some project

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import BlurIn from "../components/ui/blur-in";
 import Project1 from '../assets/Project1.png';
+import Project3 from '../assets/card3.mp4';
+import project4 from '../assets/project4.png'
 import Project2 from '../assets/Project2.mp4'
 import { FaNodeJs, FaReact, FaAws } from 'react-icons/fa';
 import { SiGraphql, SiPrisma, SiPostgresql, SiRedis, SiNextdotjs, SiTailwindcss, SiTypescript } from 'react-icons/si';
@@ -14,6 +16,7 @@ function Work() {
           <BlurIn word='Projects' />
         </h1>
         <div className="Project-container mt-20 flex flex-col space-y-20">
+          
           {/* Project 1 */}
           <div className="flex items-start text-white pb-10">
             <a href="https://twitter.abhibari.tech/" target="_blank" rel="noopener noreferrer">
@@ -42,6 +45,33 @@ function Work() {
             </div>
           </div>
 
+
+          {/* Project 3 */}
+          <div className="flex items-start text-white pb-10">
+            <a href="/" target="_blank" rel="aa">
+              <img 
+                src={Project3} 
+                className="w-[40vw] h-[20vw] mr-5" 
+                alt="Project 1" 
+              />
+            </a>
+            <div className="ml-5">
+              <h1 className="font-['NeueMontrealRegular'] text-2xl">Bookifi | Full stack</h1>
+              <p className="mt-2">Tech Stack:</p>
+              <div className="flex space-x-4 mt-2">
+                <SiTailwindcss size={30} title="Angular" />
+                <SiTailwindcss size={30} title="TailwindCSS" />
+                <SiTailwindcss size={30} title="Lenis" />
+                <SiTailwindcss size={30} title="Nodejs" />
+                <SiTailwindcss size={30} title="ExpressJS" />
+                <SiTailwindcss size={30} title="MongoDB" />
+                <FaAws size={30} title="Vercel" />
+              </div>
+              <p className="mt-4 text-lg">Designed a feature-rich booking platform for photographers, integrating React, Node.js, Express, and MongoDB for secure data handling and management and animations using GSAP. Developed a custom admin panel with secure login/logout functionality, enabling photographers to monitor booking sessions and communicate with clients directly  </p>
+            </div>
+          </div>
+
+
           {/* Project 2 */}
           <div className="flex items-start text-white">
             <div className="mr-5">
@@ -65,6 +95,32 @@ function Work() {
               />
             </a>
           </div>
+
+          {/* Project 3 */}
+          <div className="flex items-start text-white">
+            <div className="mr-5">
+              <h1 className="font-['NeueMontrealRegular'] text-2xl">Landing Page | Front End</h1>
+              <p className="mt-2">Tech Stack:</p>
+              <div className="flex space-x-4 mt-2">
+                <SiNextdotjs size={30} title="Next.js" />
+                <SiTailwindcss size={30} title="TailwindCSS" />
+                <SiTypescript size={30} title="TypeScript" />
+                <FaAws size={30} title="AWS" />
+              </div>
+              <p className="mt-4 mr-70 ">Design website that gives Interactive User Experience fully animated and interactive and add some spice using javascript, landing page equipped with smooth animations and responsive design.</p>
+            </div>
+            <a href="https://thirty-six-studio-eight.vercel.app/" target="_blank" rel="noopener noreferrer">
+              <video
+                src={card3} 
+                className="w-[100vw] h-[20vw] ml-4" 
+                controls
+                autoPlay
+                alt="Project3"
+              />
+            </a>
+          </div>
+
+
         </div>
       </div>
     </div>
